739e9403e9968ddcd261fa83bdb629e3724a1dd8
JJAE WON <55980680+ashwon12@users.noreply.github.com>
2021-03-22 03:56:27 +0900

    [Update] home layout

--- app/src/main/java/com/example/konwnow/ui/view/home/HomeFragment.kt
+++ app/src/main/java/com/example/konwnow/ui/view/home/HomeFragment.kt
@@ -1,18 +1,54 @@
 package com.example.konwnow.ui.view.home
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.Switch
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.example.konwnow.R
+import com.example.konwnow.ui.view.group.GroupActivity
 
-class HomeFragment: Fragment() {
+class HomeFragment : Fragment() {
+
+    private lateinit var v: View
+    private lateinit var switch: Switch
+    private lateinit var groupButton : TextView
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        v = inflater.inflate(R.layout.fragment_home, container, false)
+
+
+        setSwitch()
+        setButton()
+
+        return v
+    }
+
+    private fun setButton() {
+        groupButton = v.findViewById(R.id.tv_group_text)
+        groupButton.setOnClickListener {
+            activity?.let{
+                val intent = Intent(context, GroupActivity::class.java)
+                startActivity(intent)
+            }
+        }
+    }
+
+    private fun setSwitch() {
+        switch = v.findViewById(R.id.switch_hide)
+        switch.isChecked = true
+        switch.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+
+            } else {
+
+            }
+        }
     }
 }
