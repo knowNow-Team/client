b0377706f1d1cd7ec475bc0a658eb8ca38ae9dc8
GR8 <eminhwan12@gmail.com>
2021-03-23 01:31:30 +0900

    Create Test Fragment layout

---
+++ app/src/main/java/com/example/konwnow/data/FolderDTO.kt
@@ -0,0 +1,3 @@
+package com.example.konwnow.data
+
+class FolderDTO(val name: String)
---
+++ app/src/main/java/com/example/konwnow/ui/adapter/FolderAdapter.kt
@@ -0,0 +1,32 @@
+package com.example.konwnow.ui.adapter
+
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.CheckBox
+import androidx.recyclerview.widget.RecyclerView
+import com.example.konwnow.R
+import com.example.konwnow.data.FolderDTO
+
+class FolderAdapter(private val folderList: ArrayList<FolderDTO>) :
+        RecyclerView.Adapter<FolderAdapter.Holder>() {
+
+    inner class Holder(itemView: View?) : RecyclerView.ViewHolder(itemView!!) {
+        val cbFolder = itemView?.findViewById<CheckBox>(R.id.cb_folder_name)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
+        Log.d("폴더 수 ",folderList.toString())
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_folder, parent, false)
+        return Holder(view)
+    }
+
+    override fun onBindViewHolder(holder: Holder, position: Int) {
+        holder.cbFolder!!.text = folderList[position].name
+    }
+
+    override fun getItemCount(): Int {
+        return folderList.size
+    }
+}
