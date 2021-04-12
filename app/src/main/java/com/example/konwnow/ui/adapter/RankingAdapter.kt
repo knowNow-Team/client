76632c8e11cf34ea91a76d7f4f31169e86a3ca8b
GR8 <eminhwan12@gmail.com>
2021-04-13 02:36:07 +0900

    [Update] Ranking: Glide thumbnail

--- app/src/main/java/com/example/konwnow/ui/adapter/RankingAdapter.kt
+++ app/src/main/java/com/example/konwnow/ui/adapter/RankingAdapter.kt
@@ -5,9 +5,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.konwnow.R
 import com.example.konwnow.data.model.dto.Users
 
@@ -21,10 +23,14 @@ class RankingAdapter() : RecyclerView.Adapter<RankingAdapter.Holder>() {
         val tvRankingTitle = itemView?.findViewById<TextView>(R.id.tv_ranking_title)
         val tv1stLevel = itemView?.findViewById<TextView>(R.id.tv_1st_level)
         val tv1stNick = itemView?.findViewById<TextView>(R.id.tv_1st_nick)
+        val iv1stThumb = itemView?.findViewById<ImageView>(R.id.iv_1st)
         val tv2ndLevel = itemView?.findViewById<TextView>(R.id.tv_2nd_level)
         val tv2ndNick = itemView?.findViewById<TextView>(R.id.tv_2nd_nick)
+        val iv2ndThumb = itemView?.findViewById<ImageView>(R.id.iv_2nd)
         val tv3rdLevel = itemView?.findViewById<TextView>(R.id.tv_3rd_level)
         val tv3rdNick = itemView?.findViewById<TextView>(R.id.tv_3rd_nick)
+        val iv3rdThumb = itemView?.findViewById<ImageView>(R.id.iv_3rd)
+
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
@@ -48,10 +54,13 @@ class RankingAdapter() : RecyclerView.Adapter<RankingAdapter.Holder>() {
         }
         holder.tv1stLevel!!.text = String.format(myContext.getString(R.string.user_level),UserList[0].level)
         holder.tv1stNick!!.text = String.format(myContext.getString(R.string.ranking_1st), UserList[0].nick)
+        Glide.with(myContext).load(R.drawable.ic_hit).thumbnail(0.1f).into(holder.iv1stThumb!!)
         holder.tv2ndLevel!!.text = String.format(myContext.getString(R.string.user_level),UserList[1].level)
         holder.tv2ndNick!!.text = String.format(myContext.getString(R.string.ranking_2nd), UserList[1].nick)
+        Glide.with(myContext).load(R.drawable.ic_hit).into(holder.iv2ndThumb!!)
         holder.tv3rdLevel!!.text = String.format(myContext.getString(R.string.user_level),UserList[2].level)
         holder.tv3rdNick!!.text = String.format(myContext.getString(R.string.ranking_3rd), UserList[2].nick)
+        Glide.with(myContext).load(R.drawable.ic_miss).into(holder.iv3rdThumb!!)
     }
 
     override fun getItemCount(): Int {
