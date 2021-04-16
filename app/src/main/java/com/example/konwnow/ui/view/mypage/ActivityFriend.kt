7014f0e18408201153f5fe2b0ceed841297e32d9
GR8 <eminhwan12@gmail.com>
2021-04-16 15:12:40 +0900

    [Add] MypageFragment: UI

---
+++ app/src/main/java/com/example/konwnow/ui/view/mypage/ActivityFriend.kt
@@ -0,0 +1,12 @@
+package com.example.konwnow.ui.view.mypage
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.konwnow.R
+
+class ActivityFriend : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activiry_friend)
+    }
+}
---
+++ app/src/main/java/com/example/konwnow/ui/view/mypage/ActivityManual.kt
@@ -0,0 +1,13 @@
+package com.example.konwnow.ui.view.mypage
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.konwnow.R
+
+class ActivityManual : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_manual)
+    }
+}
