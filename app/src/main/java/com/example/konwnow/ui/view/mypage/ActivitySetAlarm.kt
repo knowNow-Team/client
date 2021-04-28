d137110e2eff6bef351adee91e5b033e931e9937
GR8 <eminhwan12@gmail.com>
2021-04-29 01:18:43 +0900

    [Update] Alarm: 폴더리스트 UI 변경

--- app/src/main/java/com/example/konwnow/ui/view/mypage/ActivitySetAlarm.kt
+++ app/src/main/java/com/example/konwnow/ui/view/mypage/ActivitySetAlarm.kt
@@ -2,22 +2,60 @@ package com.example.konwnow.ui.view.mypage
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.ImageButton
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.konwnow.R
+import com.example.konwnow.data.model.dto.Folder
+import com.example.konwnow.ui.adapter.FolderAdapter
 import com.google.android.material.timepicker.MaterialTimePicker
 import com.google.android.material.timepicker.MaterialTimePicker.INPUT_MODE_KEYBOARD
 import com.google.android.material.timepicker.TimeFormat
+import com.ramotion.foldingcell.FoldingCell
 
 class ActivitySetAlarm : AppCompatActivity() {
+    private lateinit var folderAdapter: FolderAdapter
+    var folderList = arrayListOf<Folder>()
+    var selectedFolderList = arrayListOf<Folder>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_alarm)
         setToolbar()
         setButton()
+        setFolderList()
+    }
+
+    private fun setFolderList() {
+        requestFolder()
+        var folderRV = findViewById<RecyclerView>(R.id.rv_folder)
+        folderRV.setHasFixedSize(true)
+        folderRV.layoutManager = LinearLayoutManager(this)
+        folderAdapter = FolderAdapter(){ i: Int, b: Boolean ->
+            if(b){
+                selectedFolderList.add(folderList[i])
+            }else{
+                selectedFolderList.remove(folderList[i])
+            }
+            for(i in selectedFolderList){
+                Log.d("선택폴더",i.name)
+            }
+        }
+
+        folderAdapter.folderUpdateList(folderList)
+        folderRV.adapter = folderAdapter
+    }
+
+    private fun requestFolder() {
+        folderList.add(Folder("name1",4))
+        folderList.add(Folder("name2",20))
+        folderList.add(Folder("name3",30))
+        folderList.add(Folder("name4",2))
+        folderList.add(Folder("name5",12))
     }
 
     private fun setButton() {
