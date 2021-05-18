9057ca3801d6bf9616c78cbf85ca8db6987639fd
JJAE WON <55980680+ashwon12@users.noreply.github.com>
2021-05-19 03:14:35 +0900

    [Add] login dto, interface

--- app/src/main/java/com/example/konwnow/data/remote/dto/Users.kt
+++ app/src/main/java/com/example/konwnow/data/remote/dto/Users.kt
@@ -2,5 +2,56 @@ package com.example.konwnow.data.remote.dto
 
 class Users{
 
-    data class SignUpBody(val google_id_token : String, val nickname : String)
+    data class SignUpBody(val nickname : String)
+
+    data class SingUpResponseBody(
+        val statusCode : Int,
+        val data : DataList,
+        val error : ErrorList?
+    )
+
+    data class ErrorList (
+        val errorCode: String,
+        val userMessage: String
+    )
+
+    data class DataList(
+        //val @id : Int,
+        val id : Int,
+        val userEmail : String,
+        val nickname: String,
+        val role:String,
+        val created : String,
+        val updated : String,
+        val userAuth : UserAuth
+    )
+
+    data class UserAuth(
+        //val @id : Int,
+        val id : Int,
+        val user : Int,
+        val loginToken : String,
+        val refreshToken: String
+    )
+
+    data class LoginResponseBody (
+        val id : Int,
+        val loginToken:String,
+        val refreshToken: String,
+        val user : LoginUserList
+    )
+
+    data class LoginUserList(
+        val created : String,
+        val id : Int,
+        val nickname : String,
+        val role :String,
+        val updated : String,
+        val userEmail : String
+    )
+
+    data class ReLoginBody(
+        val jwtToken : String,
+        val refreshToken : String
+    )
 }
--- app/src/main/java/com/example/konwnow/data/remote/retrofit/api/LoginAPi.kt
+++ app/src/main/java/com/example/konwnow/data/remote/retrofit/api/LoginAPi.kt
@@ -2,6 +2,7 @@ package com.example.konwnow.data.remote.retrofit.api
 
 import com.example.konwnow.data.remote.dto.Users
 import com.example.konwnow.utils.API
+import com.example.konwnow.utils.LOGIN
 import retrofit2.Call
 import retrofit2.http.Body
 import retrofit2.http.Header
@@ -9,10 +10,20 @@ import retrofit2.http.POST
 
 interface LoginAPi {
 
-    @POST(API.CREATE_USERS)
+    @POST(LOGIN.CREATE_USERS)
     fun postSignUp(
         @Header("google-id-token") token: String,
         @Body userRequestBody : Users.SignUpBody
-    ): Call<Users.SignUpBody>
+    ): Call<Users.SingUpResponseBody>
+
+    @POST(LOGIN.LOGIN)
+    fun postGoogleLogin(
+        @Header("google-id-token") token: String
+    ): Call<Users.LoginResponseBody>
+
+    @POST(LOGIN.RE_LOGIN)
+    fun requestNewRefreshToken(
+        @Body userRequestBody : Users.ReLoginBody
+    ): Call<Users.LoginResponseBody>
 
 }
