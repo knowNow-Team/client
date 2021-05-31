29ce116a78b8c53b56511d0ee6e7b04e6713cf36
JJAE WON <55980680+ashwon12@users.noreply.github.com>
2021-06-01 03:02:17 +0900

    [Update] 마이페이지 UI

--- app/src/main/java/com/example/konwnow/ui/view/mypage/UpdateProfileActivity.kt
+++ app/src/main/java/com/example/konwnow/ui/view/mypage/UpdateProfileActivity.kt
@@ -1,17 +1,22 @@
 package com.example.konwnow.ui.view.mypage
 
 import android.os.Bundle
+import android.widget.EditText
 import android.widget.ImageButton
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.konwnow.R
+import com.example.konwnow.ui.view.MainActivity
 
 class UpdateProfileActivity : AppCompatActivity() {
+    private lateinit var originedNick : EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_update_profile)
 
         setToolbar()
+        originedNick = findViewById(R.id.et_update_nickname)
+        originedNick.setText(MainActivity.getUserData().nickname)
     }
 
     private fun setToolbar() {
